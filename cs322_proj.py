d062e647bdefe985ce18495096514c04493ef40b
Ji Junseop <gaon0403@gmail.com>
2016-10-06 13:09:09 +0900

    Temp-proj 1-2

--- cs322_proj.py
+++ cs322_proj.py
@@ -2,17 +2,22 @@
 
 import sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 states = dict()
 symbols = []
 functions = []
 initial = str()
-final = []
+Osymbols = []
+Ofunctions = []
+# final = []
 
 def newReadline(f):
     return f.readline().strip()
 
 def getDFA():
-    fDFA = open("resource/dfa.txt", "r")
+    fDFA = open("resource/mearly.txt", "r")
 
     ### read state
     line = newReadline(fDFA)
@@ -24,6 +29,7 @@ def getDFA():
         states[state] = idx
         idx += 1
         functions.append(dict())
+        Ofunctions.append(dict())
 
     ### read symbol
     line = newReadline(fDFA)
@@ -38,9 +44,26 @@ def getDFA():
         print 'function not found'
         return 0
     line = newReadline(fDFA)
-    while line != 'Initial state' and line:
+    while line != 'Output symbol' and line:
         currS, sym, nextS = line.split(',')
         functions[states[currS]][sym] = nextS
+        line = newReadline(fDFA)
+
+    ### read output symbol
+    if line != 'Output symbol':
+        print 'output symbol not found'
+        return 0
+    Osymbols = newReadline(fDFA).split(',')
+
+    ### read output function
+    line = newReadline(fDFA)
+    if line != 'Output function':
+        print 'output function not found'
+        return 0
+    line = newReadline(fDFA)
+    while line != 'Initial state' and line:
+        currS, sym, printS = line.split(',')
+        Ofunctions[states[currS]][sym] = printS
         line = newReadline(fDFA)
 
     ### read initial state
@@ -50,36 +73,47 @@ def getDFA():
     initial = newReadline(fDFA)
 
     ### read final state
+    '''
     line = newReadline(fDFA)
     if line != 'Final state':
         print 'final state not found'
         return 0
     final = newReadline(fDFA).split(',')
+    '''
 
     fDFA.close()
-    return (states, symbols, functions, initial, final)
+    return (states, symbols, functions, initial, Osymbols, Ofunctions)
 
 
-def getInput(states, symbols, functions, initial, final):
+def getInput(states, symbols, functions, initial, Osymbols, Ofunctions):
     fInput = open("resource/input.txt", "r")
     fOutput = open("resource/output.txt", "w")
 
-    lines = fInput.readlines()
-    for i in range(len(lines)):
+    line = fInput.readline().strip()
+    while line != 'end':
         currS = initial
-        line = lines[i].strip()
+        path = ''
 
         for al in line:
             try:
+                path += Ofunctions[states[currS]][al]
                 currS = functions[states[currS]][al]
             except:
-                currS = 'function error'
+                path = 'No path exists!'
                 break
 
+        fOutput.write(path)
+        fOutput.write('\n')
+        line = fInput.readline().strip()
+
+        '''
         if currS in final:
-            print u'네'
+            fOutput.write(u'네')
+            fOutput.write('\n')
         else:
-            print u'아니요'
+            fOutput.write(u'아니요')
+            fOutput.write('\n')
+        '''
 
     fInput.close()
     fOutput.close()
@@ -90,9 +124,9 @@ if __name__ == '__main__':
     if DFAres == 0:
         print 'error while reading DFA'
         sys.exit(1)
-    states, symbols, functions, initial, final = DFAres
+    states, symbols, functions, initial, Osymbols, Ofunctions = DFAres
 
 
-    if getInput(states, symbols, functions, initial, final) > 0:
+    if getInput(states, symbols, functions, initial, Osymbols, Ofunctions) > 0:
         print 'error while reading input'
         sys.exit(1)
